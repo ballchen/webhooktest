cd26ada181e9d90eae252ae580fcdd4894cab032
ballchen <kerkerball@gmail.com>
2015-02-01 22:03:39 +0800

    yo

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.get('/', function(req, res){
 	console.log(req)
